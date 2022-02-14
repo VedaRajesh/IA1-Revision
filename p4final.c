cde81a81e16997864d54490de3e3274a69f1b51a
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-02-14 06:17:55 +0000

    done on 14.feb.2022

---
+++ p4final.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int input_array_size()
+{
+    int n;
+    printf("how many elements?\n");
+    scanf("%d",&n);
+    return n;
+}
+void input_array(int n, int a[n])
+{
+    printf("enter the array elements\n");
+    for(int i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+}
+int sum_n_arrays(int n, int a[n])
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+        {
+            sum=sum+a[i];
+        }
+    return sum;
+}
+void out_put(int n, int a[n], int sum)
+{
+    printf("sum of ");
+    for(int i=0;i<n-1;i++)
+        {
+            printf("%d +",a[i]);
+        }
+    printf("%d is %d",a[n-1],sum);
+}
+int main()
+{
+    int n,sum;
+    n=input_array_size();
+    int a[n];
+    input_array( n,  a);
+    sum= sum_n_arrays(n,a);
+    out_put(n, a,sum);
+    return 0;
+}
